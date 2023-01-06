a9a3097cf66bf0e0445e903d5601ca3f59b56773
Julia Lima <julia@MacBook-Pro-de-Julia.local>
2023-01-06 16:25:26 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `url-shortening-api-landing-page`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
